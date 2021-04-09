7107957b97261718208ee79f96d6cafb8bc186b6
hollyhbrady <hollyhbrady@gmail.com>
2021-04-09 16:56:30 +0100

    test 3 - room has number

--- tests/room_test.py
+++ tests/room_test.py
@@ -7,3 +7,9 @@ class TestRoom(unittest.TestCase):
 
     def setUp(self):
         self.room = Room(1, 10)
+
+    def test_room_has_number(self):
+        self.assertEqual(1, self.room.number)
+
+
+
