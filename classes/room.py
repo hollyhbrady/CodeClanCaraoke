0f04cea6e9dc6381cf0c37248ba211269fe3e9cb
hollyhbrady <hollyhbrady@gmail.com>
2021-04-09 17:31:17 +0100

    test 7 made

--- classes/room.py
+++ classes/room.py
@@ -3,3 +3,9 @@ class Room:
     def __init__(self, number, capacity):
         self.number = number
         self.capacity = capacity
+        self.attendees = []
+
+    # function to check in guests to room
+    # take a guest by name
+    # put in room by number
+    # assign to room list
--- tests/room_test.py
+++ tests/room_test.py
@@ -7,12 +7,17 @@ class TestRoom(unittest.TestCase):
 
     def setUp(self):
         self.room = Room(1, 10)
+        self.guest = Guest("Colin", "Rocket Man")
 
     def test_room_has_number(self):
         self.assertEqual(1, self.room.number)
 
     def test_room_has_capacity(self):
         self.assertEqual(10, self.room.capacity)
+
+    def test_check_in(self):
+        self.assertEqual("Colin", check_in(self.guest))
+
 
 
 
