99ecc4ae388f0db5cbdf6326f4adcab5387c1fff
hollyhbrady <hollyhbrady@gmail.com>
2021-04-09 16:21:41 +0100

    set up file structure and boilerplate code

---
+++ classes/guest.py
@@ -0,0 +1,5 @@
+class Guest
+
+    def __init__(self, name, fav_song):
+        self.name = name
+        self.gesture = gesture
---
+++ tests/guest_test.py
@@ -0,0 +1,9 @@
+import unittest
+from src.guest import Guest
+from src.room import Room
+from src.song import Song
+
+class TestGuest(unittest.TestCase):
+
+    def setUp(self):
+        
