339184fe07f350e0e20dd7fddb63a07b0749a1e6
hollyhbrady <hollyhbrady@gmail.com>
2021-04-10 18:02:55 +0100

    test 8 passing

--- classes/room.py
+++ classes/room.py
@@ -5,5 +5,13 @@ class Room:
         self.capacity = capacity
         self.attendees = []
 
-    def check_in(self, guest):
+    def check_in(self, guest): # tried to add a specific room to add to. Can't make it work
         self.attendees.append(guest)
+
+    def check_out(self, guest):
+        self.attendees.append(guest) # cant run without this or ValueError: list.remove(x)
+        # if guest in list: Tried to make an if loop to find guest then remove them, cant make it work
+        self.attendees.remove(guest)
+
+
+ 
--- tests/room_test.py
+++ tests/room_test.py
@@ -7,22 +7,22 @@ from classes.song import Song
 class TestRoom(unittest.TestCase):
 
     def setUp(self):
-        self.room = Room("One", 10)
+        self.room_one = Room("One", 10)
         self.guest = Guest("Colin", "Rocket Man")
 
     def test_room_has_number(self):
-        self.assertEqual("One", self.room.number)
+        self.assertEqual("One", self.room_one.number)
 
     def test_room_has_capacity(self):
-        self.assertEqual(10, self.room.capacity)
+        self.assertEqual(10, self.room_one.capacity)
 
     def test_check_in(self):
-        self.room.check_in(self.guest)
-        self.assertEqual("Colin", self.room.attendees[0].name)
+        self.room_one.check_in(self.guest)
+        self.assertEqual("Colin", self.room_one.attendees[0].name)
 
     def test_check_out(self):
-        self.room.check_out(self.guest)
-        self.assertEqual([], self.room.attendees.name)
+        self.room_one.check_out(self.guest)
+        self.assertEqual([], self.room_one.attendees)
 
     
 
